659e06160f17f38eb0b6f71fc243948791fe4ef1
Перов Алексей <p-er@yandex.ru>
2025-01-31 00:49:48 +0300

    finish operator

--- kubernetes-operators/operator/api/v1/mysql_types.go
+++ kubernetes-operators/operator/api/v1/mysql_types.go
@@ -1,18 +1,19 @@
 package v1
 
 import (
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // MySQLSpec defines the desired state of MySQL
 type MySQLSpec struct {
-    Image string `json:"image"`
-    StorageSize string `json:"storage_size"`
+	Image       string `json:"image"`
+	StorageSize string `json:"storage_size"`
+	Password    string `json:"password"`
 }
 
 // MySQLStatus defines the observed state of MySQL
 type MySQLStatus struct {
-    // Add status information here
+	// Add status information here
 }
 
 // +kubebuilder:object:root=true
@@ -20,22 +21,22 @@ type MySQLStatus struct {
 
 // MySQL is the Schema for the mysqls API
 type MySQL struct {
-    metav1.TypeMeta   `json:",inline"`
-    metav1.ObjectMeta `json:"metadata,omitempty"`
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-    Spec   MySQLSpec   `json:"spec,omitempty"`
-    Status MySQLStatus `json:"status,omitempty"`
+	Spec   MySQLSpec   `json:"spec,omitempty"`
+	Status MySQLStatus `json:"status,omitempty"`
 }
 
 // +kubebuilder:object:root=true
 
 // MySQLList contains a list of MySQL
 type MySQLList struct {
-    metav1.TypeMeta `json:",inline"`
-    metav1.ListMeta `json:"metadata,omitempty"`
-    Items           []MySQL `json:"items"`
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []MySQL `json:"items"`
 }
 
 func init() {
-    SchemeBuilder.Register(&MySQL{}, &MySQLList{})
+	SchemeBuilder.Register(&MySQL{}, &MySQLList{})
 }
--- kubernetes-operators/operator/internal/controller/mysql_controller.go
+++ kubernetes-operators/operator/internal/controller/mysql_controller.go
@@ -1,30 +1,19 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
 	"context"
+	"k8s.io/apimachinery/pkg/api/resource"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	otusv1 "github.com/Kuber-2024-10OTUS/perovmpr_repo/kubernetes-operators/operator/api/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // MySQLReconciler reconciles a MySQL object
@@ -33,30 +22,189 @@ type MySQLReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=otus.homework,resources=mysqls,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=otus.homework,resources=mysqls/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=otus.homework,resources=mysqls/finalizers,verbs=update
+//+kubebuilder:rbac:groups=otus.homework,resources=mysqls,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=otus.homework,resources=mysqls/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=otus.homework,resources=mysqls/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the MySQL object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
 func (r *MySQLReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	// Fetch the MySQL instance
+	mysql := &otusv1.MySQL{}
+	err := r.Get(ctx, req.NamespacedName, mysql)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// MySQL resource not found, likely deleted
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return ctrl.Result{}, err
+	}
+
+	// Check if the Deployment already exists, if not create a new one
+	deployment := &appsv1.Deployment{}
+	err = r.Get(ctx, req.NamespacedName, deployment)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new Deployment
+		dep := r.deploymentForMySQL(mysql)
+		log.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		err = r.Create(ctx, dep)
+		if err != nil {
+			log.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			return ctrl.Result{}, err
+		}
+	} else if err != nil {
+		log.Error(err, "Failed to get Deployment")
+		return ctrl.Result{}, err
+	}
+
+	// Check if the Service already exists, if not create a new one
+	service := &corev1.Service{}
+	err = r.Get(ctx, req.NamespacedName, service)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new Service
+		svc := r.serviceForMySQL(mysql)
+		log.Info("Creating a new Service", "Service.Namespace", svc.Namespace, "Service.Name", svc.Name)
+		err = r.Create(ctx, svc)
+		if err != nil {
+			log.Error(err, "Failed to create new Service", "Service.Namespace", svc.Namespace, "Service.Name", svc.Name)
+			return ctrl.Result{}, err
+		}
+	} else if err != nil {
+		log.Error(err, "Failed to get Service")
+		return ctrl.Result{}, err
+	}
+
+	// Check if the PVC already exists, if not create a new one
+	pvc := &corev1.PersistentVolumeClaim{}
+	err = r.Get(ctx, req.NamespacedName, pvc)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new PVC
+		pvc := r.pvcForMySQL(mysql)
+		log.Info("Creating a new PVC", "PVC.Namespace", pvc.Namespace, "PVC.Name", pvc.Name)
+		err = r.Create(ctx, pvc)
+		if err != nil {
+			log.Error(err, "Failed to create new PVC", "PVC.Namespace", pvc.Namespace, "PVC.Name", pvc.Name)
+			return ctrl.Result{}, err
+		}
+	} else if err != nil {
+		log.Error(err, "Failed to get PVC")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+// deploymentForMySQL returns a MySQL Deployment object
+func (r *MySQLReconciler) deploymentForMySQL(m *otusv1.MySQL) *appsv1.Deployment {
+	labels := labelsForMySQL(m.Name)
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Image: m.Spec.Image,
+						Name:  "mysql",
+						Env: []corev1.EnvVar{{
+							Name:  "MYSQL_ROOT_PASSWORD",
+							Value: m.Spec.Password,
+						}},
+						Ports: []corev1.ContainerPort{{
+							ContainerPort: 3306,
+							Name:          "mysql",
+						}},
+						VolumeMounts: []corev1.VolumeMount{{
+							Name:      "mysql-persistent-storage",
+							MountPath: "/var/lib/mysql",
+						}},
+					}},
+					Volumes: []corev1.Volume{{
+						Name: "mysql-persistent-storage",
+						VolumeSource: corev1.VolumeSource{
+							PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+								ClaimName: m.Name,
+							},
+						},
+					}},
+				},
+			},
+		},
+	}
+	// Set MySQL instance as the owner and controller
+	ctrl.SetControllerReference(m, dep, r.Scheme)
+	return dep
+}
+
+// serviceForMySQL returns a MySQL Service object
+func (r *MySQLReconciler) serviceForMySQL(m *otusv1.MySQL) *corev1.Service {
+	labels := labelsForMySQL(m.Name)
+	svc := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: labels,
+			Ports: []corev1.ServicePort{{
+				Port: 3306,
+				Name: "mysql",
+			}},
+			Type: corev1.ServiceTypeClusterIP,
+		},
+	}
+	// Set MySQL instance as the owner and controller
+	ctrl.SetControllerReference(m, svc, r.Scheme)
+	return svc
+}
+
+// pvcForMySQL returns a MySQL PVC object
+func (r *MySQLReconciler) pvcForMySQL(m *otusv1.MySQL) *corev1.PersistentVolumeClaim {
+	pvc := &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+		},
+		Spec: corev1.PersistentVolumeClaimSpec{
+			AccessModes: []corev1.PersistentVolumeAccessMode{
+				corev1.ReadWriteOnce,
+			},
+			Resources: corev1.VolumeResourceRequirements{
+				Requests: corev1.ResourceList{
+					corev1.ResourceStorage: resource.MustParse(m.Spec.StorageSize),
+				},
+			},
+		},
+	}
+	// Set MySQL instance as the owner and controller
+	ctrl.SetControllerReference(m, pvc, r.Scheme)
+	return pvc
+}
+
+// labelsForMySQL returns the labels for selecting the resources
+// belonging to the given MySQL CR name.
+func labelsForMySQL(name string) map[string]string {
+	return map[string]string{"app": "mysql", "mysql_cr": name}
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *MySQLReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&otusv1.MySQL{}).
+		Owns(&appsv1.Deployment{}).
+		Owns(&corev1.Service{}).
+		Owns(&corev1.PersistentVolumeClaim{}).
 		Complete(r)
 }
